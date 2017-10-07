d3c2e2570c340f8a8cd3eaf3d861fdc5cd5b02b4
bogdan23a <gbogdan23a@yahoo.com>
2017-10-08 02:33:35 +0300

    Handle connection and ping command

--- DracosBot/DracosBot/Program.cs
+++ DracosBot/DracosBot/Program.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Discord;
+using Discord.WebSocket;
 namespace DracosBot
 {
     class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+        
+        public async Task MainAsync()
         {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+            _client.MessageReceived += MessageReceived;
+
+            string token = Console.ReadLine();
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            //Block this task until the program is closed
+            await Task.Delay(-1);
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if(message.Content == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }

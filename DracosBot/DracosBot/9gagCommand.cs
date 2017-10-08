9c8ea5e3d5d99a72f98845f72c3f6229174f624b
Gherasim Marius <gcata98@gmail.com>
2017-10-08 22:30:02 +0300

    Create 9gagCommand class

---
+++ DracosBot/DracosBot/9gagCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DracosBot
+{
+    class _9gagCommand
+    {
+        private string GetRandId(string[] command)
+        {  
+
+            return null;
+
+        }
+
+        private static string GetPicture()
+        {
+            return "abc";
+
+        }
+
+        public static string[] command = {"!9gaghot","!9gagfresh","!9gagtrending"};
+        public static string answer=GetPicture();
+    }
+}

f1fa958a8d0e7b310e32adf1f2625d673833945d
bogdan23a <gbogdan23a@yahoo.com>
2017-10-08 03:25:11 +0300

    Token storing logic and build

--- DracosBot/DracosBot/Program.cs
+++ DracosBot/DracosBot/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Discord;
 using Discord.WebSocket;
+
 namespace DracosBot
 {
     class Program
@@ -19,7 +22,72 @@ namespace DracosBot
             _client.Log += Log;
             _client.MessageReceived += MessageReceived;
 
-            string token = Console.ReadLine();
+            //TOKEN STORING LOGIC 
+            //Store the token in a txt file to avoid hardcoding it 
+            //User confirmation needed
+            string assumedToken = string.Empty;
+            string token = string.Empty;
+            bool hasTokenChanged = false;
+            
+            //First check if file exists 
+            if (File.Exists("token.txt"))
+            {
+                //read the token and ask the user to confirm it 
+                //if it is not the same token keep asking for the token until it is the right one
+
+                assumedToken = File.ReadAllText("token.txt");
+                Console.WriteLine("Confirm token : " + assumedToken);
+                Console.WriteLine("Y/N");      
+                
+                while (Console.ReadLine() == "N")
+                {
+                    hasTokenChanged = true;
+                    Console.WriteLine("Enter your token below :");
+                    assumedToken = Console.ReadLine();
+                    Console.WriteLine("Confirm token : " + assumedToken);
+                    Console.WriteLine("Y/N");
+                }
+                //finally assign the true value for the token
+                //and if the token changed update the file
+
+                token = assumedToken;
+                if (hasTokenChanged)
+                    using (StreamWriter stream = new StreamWriter("myfile.txt"))
+                    {
+                        stream.WriteLine(token);
+                    }
+            }
+            //if it does not exist it means it's the first time the user boots 
+            else
+            {
+                //ask for the token and the for the confirmation 
+                //if it is wrong keep asking for it until it is the right one
+
+                Console.WriteLine("Enter your token below :");
+                assumedToken = Console.ReadLine();
+                Console.WriteLine("Confirm token : " + assumedToken);
+                Console.WriteLine("Y/N");
+
+                while(Console.ReadLine() == "N")
+                {
+                    Console.WriteLine("Enter your token below :");
+                    assumedToken = Console.ReadLine();
+                    Console.WriteLine("Confirm token : " + assumedToken);
+                    Console.WriteLine("Y/N");
+                }
+                //finally assign the true value for the token
+                //also create the file to store the token and write the token to it
+                token = assumedToken;
+                var tokenFile = File.Create("token.txt");
+                tokenFile.Close();
+
+                using (StreamWriter stream = new StreamWriter("token.txt"))
+                {
+                    stream.WriteLine(token);
+                }
+            }
+
+
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
 

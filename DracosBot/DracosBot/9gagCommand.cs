ed4f3b7180b2f28d02cb85d804ebd52bc75eb23c
Gherasim Marius <gcata98@gmail.com>
2017-10-11 00:14:47 +0300

    _9gagCommand Class version 1.0

--- DracosBot/DracosBot/9gagCommand.cs
+++ DracosBot/DracosBot/9gagCommand.cs
@@ -3,25 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace DracosBot
 {
     class _9gagCommand
     {
-        private string GetRandId(string[] command)
-        {  
-
-            return null;
-
-        }
-
-        private static string GetPicture()
+        public static int Index;
+        public static string[] command = { "!9gaghot", "!9gagtrending", "!9gagfresh" };
+        public static string answer = GetRandIdHot(Index);
+        public static string[] GagLink = { "https://9gag.com/hot", "https://9gag.com/trending", "https://9gag.com/fresh" };
+        private static string GetRandIdHot(int Index)
         {
-            return "abc";
+            WebClient  GagUrl= new WebClient();
+            string GagSource = string.Empty;
+            string GagPostSource = string.Empty;
+            try
+            {
+                GagSource = GagUrl.DownloadString(GagLink[Index]);
 
+            }
+            catch(Exception e)
+            {
+               
+            }
+
+            string ArticleMatch = "img class=\"(.*?)\"(.*?)\"(.*?)\"";
+            Random randomGag = new Random();
+            int randomGagint = randomGag.Next(1, 10);
+            int IsRandomGag = 0;
+
+            foreach(Match match in Regex.Matches(GagSource,ArticleMatch))
+            {
+                IsRandomGag++;
+                if(IsRandomGag==randomGagint)
+                {
+                    GagPostSource = match.Groups[3].Value;
+                    break;
+                } 
+            }
+            string ImgName = GagPostSource.Split('/')[4];
+            string ImgPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string ImgPatha = ImgPath.Remove(ImgPath.Length - 13) + ImgName;
+            using (GagUrl)
+            {
+                GagUrl.DownloadFile(new Uri(GagPostSource),ImgPatha);
+            }
+            return ImgPatha;
         }
-
-        public static string[] command = {"!9gaghot","!9gagfresh","!9gagtrending"};
-        public static string answer=GetPicture();
     }
 }

fa628a7681cc069ba04f0ed5a05e1a1f80049c29
bogdan23a <gbogdan23a@yahoo.com>
2017-10-28 17:40:39 +0300

    smr

--- DracosBot/DracosBot/9gagCommand.cs
+++ DracosBot/DracosBot/9gagCommand.cs
@@ -10,18 +10,8 @@ namespace DracosBot
 {
     class _9gagCommand
     {
-        public static int Index;
-        public static string[] command = { "!9gaghot", "!9gagtrending", "!9gagfresh" };
-        public static string answer = GetRandIdHot(Index);
-        public static string[] GagLink = { "https://9gag.com/hot", "https://9gag.com/trending", "https://9gag.com/fresh" };
-        private static string GetRandIdHot(int Index)
-        {
-            WebClient  GagUrl= new WebClient();
-            string GagSource = string.Empty;
-            string GagPostSource = string.Empty;
-            try
-            {
-                GagSource = GagUrl.DownloadString(GagLink[Index]);
+        private string GetRandId(string[] command)
+        {  
 
             }
             catch(Exception e)
@@ -29,28 +19,10 @@ namespace DracosBot
                
             }
 
-            string ArticleMatch = "img class=\"(.*?)\"(.*?)\"(.*?)\"";
-            Random randomGag = new Random();
-            int randomGagint = randomGag.Next(1, 10);
-            int IsRandomGag = 0;
+        private static string GetPicture()
+        {
+            return "abc";
 
-            foreach(Match match in Regex.Matches(GagSource,ArticleMatch))
-            {
-                IsRandomGag++;
-                if(IsRandomGag==randomGagint)
-                {
-                    GagPostSource = match.Groups[3].Value;
-                    break;
-                } 
-            }
-            string ImgName = GagPostSource.Split('/')[4];
-            string ImgPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            string ImgPatha = ImgPath.Remove(ImgPath.Length - 13) + ImgName;
-            using (GagUrl)
-            {
-                GagUrl.DownloadFile(new Uri(GagPostSource),ImgPatha);
-            }
-            return ImgPatha;
         }
     }
 }

afffe21110f4c30f0fe7e854f6fadf230819f971
bogdan23a <gbogdan23a@yahoo.com>
2017-10-29 15:20:32 +0200

    well

---
+++ DracosBot/DracosBot/Music/MusicHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 
+namespace DracosBot.Music
+{
+    using AudioLoadResultHandler = com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+    using AudioPlayerManager = com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+    using DefaultAudioPlayerManager = com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+    using AudioSourceManagers = com.sedmelluq.discord.lavaplayer.source.AudioSourceManager;
+    using FriendlyException = com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+    using AudioPlaylist = com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+    using AudioTrack = com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
+
+    public class MusicHandler
+    {
+        private readonly AudioPlayerManager playerManager;
+        private readonly Map<long, GuildMusicManager>
+    }
+}
